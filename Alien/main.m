e831e5ac48f82331c96fc55fcaebd9c90b956e26
Paul Warner <paul.eliot.warner@gmail.com>
2017-10-02 19:33:03 -0400

    Initial Commit

---
+++ Alien/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Alien
+//
+//  Created by Paul Warner on 10/2/17.
+//  Copyright © 2017 Paul Warner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
